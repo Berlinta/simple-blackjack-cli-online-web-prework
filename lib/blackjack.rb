abf34b6bad3bd7abcebdcb5374a3cff698b1ecc1
Nash Brancic <nashfft@gmail.com>
2019-01-27 00:25:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,12 +49,12 @@ end
 #####################################################
 
 def runner
-  Welcome
+  welcome
   hand=initial_round
   until hand>21 do
     hand=hit?(hand)
     display_card_total(hand)
   end
-end_game(hand)
+  end_game(hand)
 end
 
